9855509230c6f14f7e4040f396866ed6aaae55d1
Narin Sundarabhaya <nrs710@gmail.com>
2017-05-19 23:19:13 -0700

    added additional pseudo code & click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,43 +18,94 @@
 // if player HP is <= 0, lose game
 
 
+// Global Variables
+
+var charHp = 0;
+var enemHp = 0;
+
+
+// Character Objects
+
+var playerOne = {
+    name: 'Homer',
+    hp: 110,
+    attackPower: 20,
+    counterAttack: 5,
+}
+
+var playerTwo = {
+    name: 'Bart',
+    hp: 125,
+    attackPower: 13,
+    counterAttack: 10,
+}
+
+var playerThree = {
+    name: 'Lisa',
+    hp: 105,
+    attackPower: 15,
+    counterAttack: 5,
+}
+
+var playerFour = {
+    name: 'Marge',
+    hp: 115,
+    attackPower: 10,
+    counterAttack: 15,
+}
+
+
+
+var startGame = function() {
+
+    // click event when selecting your character
+    $('.boxDescription').click(function() {
+        // when character is selected 1. move to player-area 2. change bg to black
+        $(this).appendTo("#player-area");
+        $(this).css({ "background-color": "black", "color": "white" });
+
+        // other 3 characters move to enemies-section
+
+        // choose 1 of 3 for 1st enemy
+
+        // move enemy to #enemies-attack
+
+    });
+
+    // add remaining characters to enemies-attack area
+    $('.boxDescription').click(function() {
+        $(!this).appendTo("#enemies-attack");
+    })
+
+    // attack button function
+    $(".btn-danger").click(function() {
+        // decrement enemy hp by attackPower
+        // increment attackPower by original attackPower each time attacking
+
+        // decremeant player hp by enemy attackPower
+
+
+    })
+
+    // function for next enemy
+
+    // if player hp <= 0 - lose game
+
+    // if enemy 1 hp <= 0, remove from game, player hp stays in memory
+    // player chooses next opponent
+    // if enemy 2 hp <= 0, remove from game, player hp stays in memory
+    // player choses final opponent
+    // if final enemy <= 0, you win!
+
+}; // end startGame()
 
 
 // starts game at page load
 $(document).ready(function() {
 
-    // Global Variables
+    startGame();
 
 
-    // Character Objects
-
-    var playerOne = {
-        name: 'Homer',
-        hp: 110,
-        attackPower: 20,
-        counterAttack: 5,
-    }
-
-    var playerTwo = {
-        name: 'Bart',
-        hp: 125,
-        attackPower: 13,
-        counterAttack: 10,
-    }
-
-    var playerThree = {
-        name: 'Lisa',
-        hp: 105,
-        attackPower: 15,
-        counterAttack: 5,
-    }
-
-    var playerFour = {
-        name: 'Marge',
-        hp: 115,
-        attackPower: 10,
-        counterAttack: 15,
-    }
 
     // end document.ready()
 });
