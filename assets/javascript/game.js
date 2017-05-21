45276bd28e08b69e6c411480d6c73cddb0e0f98f
Narin Sundarabhaya <nrs710@gmail.com>
2017-05-20 22:48:44 -0700

    more work on click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,7 +25,7 @@ var enemHp = 0;
 var character = "";
 
 
-// Character Objects
+// Character Objects stored into array
 
 characterOptions = [
 
@@ -34,12 +34,14 @@ characterOptions = [
         hp: 110,
         attackPower: 20,
         counterAttack: 5,
+        numberOfAttacks: 1
     },
     playerTwo = {
         name: 'Bart',
         hp: 125,
         attackPower: 13,
         counterAttack: 10,
+        numberOfAttacks: 1,
     },
 
     playerThree = {
@@ -47,12 +49,14 @@ characterOptions = [
         hp: 105,
         attackPower: 15,
         counterAttack: 5,
+        numberOfAttacks: 1
     },
     playerFour = {
         name: 'Marge',
         hp: 115,
         attackPower: 10,
         counterAttack: 15,
+        numberOfAttacks: 1
     }
 ]
 
@@ -73,7 +77,6 @@ var startGame = function() {
     $('.boxDescription').click(function() {
         // when character is selected 1. move to player-area 2. change bg to black
         var $this = $(this);
-
         $this.addClass('selected');
         $this.removeClass('unselected');
         $(this).appendTo("#player-area");
@@ -84,23 +87,34 @@ var startGame = function() {
         // other 3 characters move to enemies-section
         $('.boxDescription:not(.selected)').appendTo("#enemies-attack");
 
-        // choose 1 of 3 for 1st enemy
         $('.unselected').click(function() {
-                $(this).addClass('defender');
-                // move enemy to #enemies-attack
-                $(this).appendTo('#fight-section');
-                // store defender object data
-                yourDefender = characterOptions[$(this).data('player') - 1];
-                console.log('your opponent: ' + yourDefender.name);
-            }) // end enenmy click
+            $('#enemies-attack').addClass('defender');
+            // move enemy to #enemies-attack
+            $(this).appendTo('#fight-section');
+            // store defender object data
+            yourDefender = characterOptions[$('.unselected').data('player') - 1];
+            console.log('your opponent: ' + yourDefender.name);
+        });
+
     }); // end character click
 
+    // choose 1 of 3 for 1st enemy
+
+
     // attack button function
-    $(".btn-danger").click(function() {
+    $(".btn-danger").click(function(yourCharacter, yourDefender) {
+
+        var attacksIncrement = yourCharacter.numberOfAttacks++;
+        var attackPowerIncrement = attacksIncrement * yourCharacter.attackPower;
+        yourDefender.hp -= attackPowerIncrement;
+        yourDefender.element.find('hp').html(yourDefender.hp);
+
+
+
         // decrement enemy hp by attackPower
-        yourCharacterhp = yourCharacter.hp - yourDefender.hp;
-        console.log("your HP" + yourCharacter.hp + " should subtract " + yourDefender.hp);
-        console.log('Hit! Your HP should be: ' + yourCharacter);
+        yourCharacterhp = yourCharacter.hp - yourDefender.attackPower;
+        yourDefenderhp = yourDefender.hp - yourCharacter.attackPower;
+        console.log("your HP: " + yourCharacterhp + " your defender HP: " + yourDefenderhp);
     });
     // increment attackPower by original attackPower each time attacking
 
